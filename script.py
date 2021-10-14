0ef37a8ea04d46acef0ce6ff92def9b0555400d4
Xiu <xiaowenyuan@users.noreply.github.com>
2021-10-15 00:14:48 +0800

    Added the actual gameplay

--- script.py
+++ script.py
@@ -1,20 +1,40 @@
 from random import randrange
+from math import sqrt
 
-def generate_grid(n):
+def generate_grid(grid, player_dictionary):
     count = 1
-    for row in range(1,n+1):
+    row_length = int(sqrt(len(grid)))
+    for row in range(1, row_length+1):
         row_for_print = ""
-        for square in range(1,n+1):
-            if row == 1 and square <10:
-                count_for_print = f"|   0{count}   |"
+        for square in range(1,row_length+1):
+            player_token = ""
+            if grid[count-1].occupied():
+                list_of_players = grid[count-1].obtain_players()
+                for player in list_of_players:
+                    player_token += player
+            if grid[count-1].query_is_free():
+                if row == 1 and square <10:
+                    count_for_print = f"|      {player_token}{count}     |"
+                elif row > 9 and square > 9:
+                    count_for_print = f"|     {player_token}{count}    |"
+                elif row > 10:
+                    count_for_print = f"|     {player_token}{count}    |"
+                else:
+                    count_for_print = f"|      {player_token}{count}     |"
             else:
-                count_for_print = f"|   {count}   |"
+                if grid[count-1].query_if_snake_head():
+                    count_for_print = f"|    {player_token}{count}SH{str(grid[count-1].snake_tail_square.get_number())}   |"
+                elif grid[count-1].query_if_snake_tail():
+                    count_for_print = f"|     {player_token}{count}ST    |"
+                elif grid[count-1].query_if_ladder_bottom():
+                    count_for_print = f"|    {player_token}{count}LB{str(grid[count-1].ladder_top_square.get_number())}   |"
+                else:
+                    count_for_print = f"|    {player_token}{count}LT    |"
+
             row_for_print += count_for_print
             count+=1
         print(row_for_print)
         print("\n")
-
-#generate_grid(10)
 
 class Square:
     def __init__(self, number, snake_head = False, ladder_bottom = False, start = False, end = False):
@@ -27,6 +47,7 @@ class Square:
         self.is_ladder_top = False
         self.snake_tail_square = None
         self.ladder_top_square = None
+        self.occupied_by_players = []
     
     def __repr__(self):
         to_be_returned = str(self.number)
@@ -40,6 +61,21 @@ class Square:
             to_be_returned += "LT" 
         return to_be_returned
 
+    def stepped_on(self, player):
+        self.occupied_by_players.append(player)
+
+    def stepped_off(self, player):
+        self.occupied_by_players = [x for x in self.occupied_by_players if x is not player]
+
+    def obtain_players(self):
+        return self.occupied_by_players
+    
+    def occupied(self):
+        if len(self.occupied_by_players) == 0:
+            return False
+        else:
+            return True
+
     def get_number(self):
         return self.number
 
@@ -49,10 +85,16 @@ class Square:
         tail.set_snake_tail()
         
     def set_snake_tail(self):
-        self.snake_tail = True 
+        self.is_snake_tail = True 
+    
+    def give_snake_tail_square_number(self):
+        return self.snake_tail_square.get_number()
+
+    def give_ladder_top_square_number(self):
+        return self.ladder_top_square.get_number()
     
     def set_ladder_top(self):
-        self.ladder_top = True
+        self.is_ladder_top = True
 
     def set_ladder_bottom(self, top):
         self.is_ladder_bottom = True
@@ -88,6 +130,12 @@ class Square:
             return False
         return True
     
+    def reach_end(self):
+        if self.end:
+            return True
+        else:
+            return False
+    
 def generate_squares(n):
     list_of_squares = []
     for i in range(1,n**2+1):
@@ -100,32 +148,135 @@ def generate_squares(n):
         list_of_squares.append(square_i)
     return list_of_squares
 
-sample_board = generate_squares(10)
-
 def set_board_snakes(n, list_of_squares):
     for i in range(1, n+1):
-        snake_head = randrange(11, len(list_of_squares))
-        while not list_of_squares[snake_head].query_is_free():
-            snake_head = randrange(11, len(list_of_squares))
-        snake_tail = randrange(1, list_of_squares[snake_head].get_number())
-        while not list_of_squares[snake_tail].query_is_free():
-            snake_tail = randrange(1, list_of_squares[snake_head].get_number())
+        snake_head = randrange(10, len(list_of_squares)-1) - 1
+        is_snake_head_square_free = list_of_squares[snake_head].query_is_free()
+        while not is_snake_head_square_free:
+            snake_head = randrange(11, len(list_of_squares)-1) - 1
+            is_snake_head_square_free = list_of_squares[snake_head].query_is_free()
+        snake_tail = randrange(2, list_of_squares[snake_head].get_number()) - 1
+        is_snake_tail_square_free = list_of_squares[snake_tail].query_is_free()
+        while not is_snake_tail_square_free:
+            snake_tail = randrange(2, list_of_squares[snake_head].get_number()) - 1
+            is_snake_tail_square_free = list_of_squares[snake_tail].query_is_free()
         list_of_squares[snake_head].set_snake_head(list_of_squares[snake_tail])
 
 def set_board_ladders(n, list_of_squares):
     for i in range(1, n+1):
         #randomise square that will be the bottom of the ladder
-        ladder_bottom = randrange(1, len(list_of_squares) - 10)
+        ladder_bottom = randrange(2, len(list_of_squares) - 10) - 1
+        is_ladder_bottom_square_free = list_of_squares[ladder_bottom].query_is_free()
         #check if the square already is a snake head, snake tail, or another ladder top or bottom
-        #while not list_of_squares[ladder_bottom].query_is_free():
-            #ladder_bottom = randrange(1, len(list_of_squares) - 10)
+        while not is_ladder_bottom_square_free:
+            ladder_bottom = randrange(2, len(list_of_squares) - 10) - 1
+            is_ladder_bottom_square_free = list_of_squares[ladder_bottom].query_is_free()
         #randomise square that will be the top of the ladder
-        ladder_top = randrange(ladder_bottom+10, len(list_of_squares))
+        ladder_top = randrange(ladder_bottom+10, len(list_of_squares)-1) - 1
+        is_ladder_top_square_free = list_of_squares[ladder_top].query_is_free()
         #check if the square is already a snake head, snake tail or bottom of the ladder
-        #while not list_of_squares[ladder_top].query_is_free():
-            #ladder_top = randrange(ladder_bottom+10, len(list_of_squares))
+        while not is_ladder_top_square_free:
+            ladder_top = randrange(ladder_bottom+10, len(list_of_squares)-1) - 1
+            is_ladder_top_square_free = list_of_squares[ladder_top].query_is_free()
         #use the class method to set the ladder for the square
         list_of_squares[ladder_bottom].set_ladder_bottom(list_of_squares[ladder_top])
-set_board_snakes(5, sample_board)
-set_board_ladders(5, sample_board)
-print(sample_board)
+
+def dice_roll(number_of_dice):
+    result = 0
+    for dice in range(1, number_of_dice+1):
+        result += randrange(1,7)
+    return result
+
+#the start_game function initialises the dictionary and 1st grid 
+def start_game_for_each_player(player_dictionary, player, grid):
+    #every player starts at square 1
+    grid[0].stepped_on(player)
+    #update players dictionary to reflect that each player is at grid1
+    player_dictionary[player] = grid[0]
+
+
+def each_turn(players, number_of_dice, grid):
+    #gameplay is looped between each player
+    for player in players.keys():
+        print(f'Player {player}\'s turn.')
+        #check what square the player is currently on 
+        current_square = players[player]
+        current_square_number = players[player].get_number()
+        print(f'Player {player} is currently at square {current_square_number}')
+        #each player starts by rolling the dice
+        player_to_continue = input(f'Is Player {player} ready to roll the dice? Type Y to continue.') 
+        while player_to_continue.lower() != 'y':
+            player_to_continue = input(f'Is Player {player} ready to roll the dice? Type Y to continue.')
+        dice_result = dice_roll(number_of_dice)
+        print(f'Player {player} rolled a {dice_result}')
+        #step off the current square 
+        current_square.stepped_off(player)
+        #move to the new square according to the dice result
+        new_square_number = current_square_number + dice_result
+        new_square_index = new_square_number - 1
+        #check if new square number is below the max
+        if new_square_number < len(grid):
+            new_square = grid[new_square_index] 
+            #check if square is ladder bottom or snake head
+            if new_square.query_if_snake_head():
+                print(f'Oh no! Player stepped on snake head at square {new_square_number}')
+                snake_tail_square_number = new_square.give_snake_tail_square_number()
+                new_square_number = snake_tail_square_number
+                new_square_index = new_square_number - 1
+                new_square = grid[new_square_index]
+            elif new_square.query_if_ladder_bottom():
+                print(f'Awesome! Player stepped on ladder at square {new_square_number}')
+                ladder_top_square_number = new_square.give_ladder_top_square_number()
+                new_square_number = ladder_top_square_number
+                new_square_index = new_square_number - 1
+                new_square = grid[new_square_index]
+            #player steps on the correct square and update the dictionary accordingly
+            new_square.stepped_on(player)
+            players[player] = new_square
+            #print out what square the player is now on 
+            print(f'Player {player} has moved to square {new_square_number}')
+            generate_grid(grid, players)
+        else:
+            print(f'Player {player} has reached the end.')
+            print(f'Player {player} has won the game.')
+            return True
+    return False
+
+
+def run_game():
+    print('Welcome to Snakes and Ladders!')
+    number_of_players = input('How many players are playing? Please insert a number between 1 to 4')
+    while int(number_of_players) < 1 or int(number_of_players) > 4:
+        number_of_players = input('How many players are playing? Please insert a number between 1 to 4')
+    grid_number = input('What size of a board would you like? Please insert a number n to represent nxn board.')
+    gameboard = generate_squares(int(grid_number))
+    number_of_snakes_ladders = input(f'How many sets of snakes and ladders would you like on the board? Please insert a number between 1 to {grid_number}')
+    while int(number_of_players) < 1 or int(number_of_players) > int(grid_number):
+        number_of_snakes_ladders = input(f'How many sets of snakes and ladders would you like on the board? Please insert a number between 1 to {grid_number}')
+    set_board_snakes(int(number_of_snakes_ladders), gameboard)
+    set_board_ladders(int(number_of_snakes_ladders), gameboard)
+    print('Generating your board...')
+    print(gameboard)
+    number_of_dice = input('How many dies would you like to use? Insert a number between 1 and 2.')
+    while int(number_of_dice) < 1 or int(number_of_dice) > 2:
+        number_of_dice = input('How many dies would you like to use? Insert a number between 1 and 2.')
+    player_tokens_list = ['!', '@', '#', "?", '%', '&', '$', '*']
+    players_and_grid = {}
+    for i in range(1, int(number_of_players)+1):
+        player_token = input(f'Choose a token to represent Player {i}: {str(player_tokens_list)}')
+        player_tokens_list = [x for x in player_tokens_list if x is not player_token]
+        start_game_for_each_player(players_and_grid, player_token, gameboard)
+    print(f'Our players are:')
+    for player in players_and_grid.keys():
+        print(player + " ")
+    generate_grid(gameboard, players_and_grid)
+    game_over = False
+    while not game_over:
+        game_over = each_turn(players_and_grid, int(number_of_dice), gameboard)
+    print('Game over. Thank you for playing.')
+
+run_game()
+
+        
+
+        
